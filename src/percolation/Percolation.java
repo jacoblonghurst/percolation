36fb896bff2a48b463d6e9e26811054770adccb4
Jacob Longhurst <jacoblonghurst@gmail.com>
2018-01-22 18:27:17 -0700

    Updated perc

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -1,5 +1,33 @@
 package percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    
+
+    private boolean[][] grid;
+
+    private int lenSize;
+
+    private WeightedQuickUnionUF topUf;
+    private WeightedQuickUnionUF botUf;
+
+    private int topIndex, bottomIndex;
+
+    public Percolation(int N) {
+
+        if(N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        grid = new boolean[N][N];
+
+        lenSize = N;
+
+        topUf = new WeightedQuickUnionUF(lenSize * lenSize + 1);
+        botUf = new WeightedQuickUnionUF(lenSize * lenSize + 1);
+
+        topIndex = 0;
+
+        bottomIndex = 0;
+    }
 }
