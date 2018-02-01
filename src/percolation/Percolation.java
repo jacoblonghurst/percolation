f91b5b8c90b14a7f1bea46e0aab5efd4aa842aca
Jacob Longhurst <jacoblonghurst@gmail.com>
2018-01-31 17:02:33 -0700

    For some reason this didn't commit

--- src/percolation/Percolation.java
+++ src/percolation/Percolation.java
@@ -11,7 +11,7 @@ public class Percolation {
     private WeightedQuickUnionUF topUf;
     private WeightedQuickUnionUF botUf;
 
-    private int topIndex, bottomIndex;
+    private int topInd, botInd;
 
     public Percolation(int N) {
 
@@ -26,10 +26,82 @@ public class Percolation {
         topUf = new WeightedQuickUnionUF(lenSize * lenSize + 1);
         botUf = new WeightedQuickUnionUF(lenSize * lenSize + 1);
 
-        topIndex = 0;
+        topInd = 0;
 
-        bottomIndex = 0;
-        
-        int temp = 0;
+        botInd = 0;
+    }
+
+    public void open(int i, int j) {
+        if(i >= lenSize + 1 || j >= lenSize + 1 || i < 0 || j < 0) {
+            throw new IndexOutOfBoundsException("Illegal point of (" + i + ", " + j + ") given to open()");
+        } else {
+
+            grid[i - 1][j - 1] = true;
+
+            if(i > 1 && isOpen(i - 1, j)) {
+                topUf.union(getUF(i, j), getUF(i - 1, j));
+                botUf.union(getUF(i, j), getUF(i - 1, j));
+            }
+
+            if(i < lenSize - 1 && isOpen(i + 1, j)) {
+                topUf.union(getUF(i, j), getUF(i + 1, j));
+                botUf.union(getUF(i, j), getUF(i + 1, j));
+            }
+
+            if(j < lenSize && isOpen(i, j + 1)) {
+                topUf.union(getUF(i, j), getUF(i, j + 1));
+                botUf.union(getUF(i, j), getUF(i, j + 1));
+            }
+
+            if(j > 1 && isOpen(i, j - 1)) {
+                topUf.union(getUF(i, j), getUF(i, j - 1));
+                botUf.union(getUF(i, j), getUF(i, j - 1));
+            }
+
+            if (i == 1) {
+                topUf.union(getUF(i, j), topInd);
+            }
+
+            if (i == lenSize) {
+                botUf.union(getUF(i, j), botInd);
+            }
+        }
+    }
+
+    public boolean isOpen(int i, int j) {
+        if(i >= lenSize + 1 || j >= lenSize + 1 || i < 0 || j < 0) {
+            throw new IndexOutOfBoundsException("Illegal point of (" + i + ", " + j + ") given to isOpen()");
+        } else {
+
+            // returns if it is open or not
+            return grid[i - 1][j - 1];
+        }
+    }
+
+
+    public boolean isFull(int i, int j) {
+        if(i >= lenSize + 1 || j >= lenSize + 1 || i < 0 || j < 0) {
+            throw new IndexOutOfBoundsException("Illegal point of (" + i + ", " + j + ") given to isFull()");
+        } else {
+            // returns if there is a path from this point to the virtual top point
+            return topUf.connected(topInd, getUF(i, j));
+        }
+    }
+
+    public boolean percolates() {
+        // sees if it percolates by seeing if there is a point in the bottom row that connects to both virtual spots
+
+        for(int i = 0; i < lenSize; i++) {
+            if(topUf.connected(getUF(lenSize, i), topInd) && botUf.connected(getUF(lenSize, i), botInd)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // private method that changes our 3d grid point into a 2d index used by UF
+    private int getUF(int i, int j) {
+        return lenSize * (i - 1) + j;
     }
 }
--- src/percolation/PercolationStats.java
+++ src/percolation/PercolationStats.java
@@ -1,4 +1,80 @@
 package percolation;
 
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+import java.util.Scanner;
+
+
 public class PercolationStats {
+    /**
+     * Declare the variables being use
+     */
+
+    private Percolation perc; // Declare the percolation variable
+    private double[] stats; // Keep track of the statistics we are finding out
+    private int expCount;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        StdOut.print("Please input the grid size with a number: ");
+        int N = input.nextInt();
+        StdOut.print("Next input the number of times you want to run the experiment: ");
+        int T = input.nextInt();
+        StdOut.printf("%n%n%s%n", "Great hold on while we crunch the numbers.");
+        PercolationStats percStats = new PercolationStats(N, T);
+        StdOut.printf("The mean is: %f%nThe standard deviation is: %f%nThe confidence low is: %f%nThe confidence high is: %f",
+                percStats.mean(), percStats.stddev(), percStats.confidenceLow(), percStats.confidenceHigh());
+        input.close();
+    }
+
+    /**
+     * @param N defines the grid size N * N
+     * @param T defines the number of times to run the statistics
+     */
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Grid and/or number of experiments was <= 0");
+        }
+        expCount = T;
+        stats = new double[T];
+        for (int i = 0; i < T; i++) {
+            int opened = 0;
+            perc = new Percolation(N);
+            while (!perc.percolates()) {
+                int j = StdRandom.uniform(1, 1 + N);
+                int k = StdRandom.uniform(1, 1 + N);
+                if (!perc.isOpen(j, k)) {
+                    perc.open(j, k);
+                    opened++;
+                }
+            }
+            double tempStat = (double) opened / (N * N); // this will get the ratio of opened sites to the whole grid
+            stats[i] = tempStat;
+        }
+
+    }
+
+    /**
+     * @return the mean of the percolation
+     */
+    public double mean() {
+        return StdStats.mean(stats);
+    }
+
+    /**
+     * @return the standard deviation
+     */
+    public double stddev() {
+        return StdStats.stddev(stats);
+    }
+
+    public double confidenceLow() {
+        return this.mean() - ((1.96 * this.stddev()) / Math.sqrt(expCount));
+    }
+
+    public double confidenceHigh() {
+        return this.mean() + ((1.96 * this.stddev()) / Math.sqrt(expCount));
+    }
 }
